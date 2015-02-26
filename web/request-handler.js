b2c5eb4166f9810de950071eca7f87d17a73bcde
garronmichael <garron.michael@gmail.com>
2015-02-25 17:30:03 -0800

    Refactor

--- web/request-handler.js
+++ web/request-handler.js
@@ -6,21 +6,27 @@ var httpHelpers = require('./http-helpers');
 exports.handleRequest = function (req, res) {
   //res.end(archive.paths.list);
 
-  if (req.method === 'GET') {
-    var asset;
-     if(req.url === '/') {
-      asset = path.resolve('web/public', 'index.html');
-    } else {
-      asset = path.resolve('web', '../archives/sites' + req.url);
-    }
-    console.log(asset);
-    httpHelpers.serveAssets(res, asset, function (err, data) {
-      // if (err) {
-      //   throw err;
-      // }
-      res.end(data);
-    });
-  }
+  var actions = {
+    'GET': function(req, res) {
+      var asset;
+       if(req.url === '/') {
+        asset = path.resolve('web/public', 'index.html');
+      } else {
+        asset = path.resolve('web', '../archives/sites' + req.url);
+      }
+      console.log(asset);
+      httpHelpers.serveAssets(res, asset, function (err, data) {
+        // if (err) {
+        //   throw err;
+        // }
+        res.end(data);
+      });
+    },
+    'POST': function () {
 
+    }
+  };
+
+  actions[req.method](req, res);
 
 };
