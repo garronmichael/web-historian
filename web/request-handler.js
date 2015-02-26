447c6b3627b6c8d9bcfc139b85a900e02d777bf2
garronmichael <garron.michael@gmail.com>
2015-02-25 16:59:34 -0800

    Passed first test

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,23 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  //res.end(archive.paths.list);
+
+  if (req.method === 'GET') {
+
+
+    httpHelpers.serveAssets(res, path.resolve('/users', 'index.html'), function (err, data) {
+
+      console.log(path.resolve('public', 'index.html'));
+      // if (err) {
+      //   throw err;
+      // }
+      res.end(data);
+    });
+  }
+
+
 };
