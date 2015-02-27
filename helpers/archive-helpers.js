1c68a4b8a9a71a1d52b3f6b062814aa768aa6bff
garronmichael <garron.michael@gmail.com>
2015-02-26 20:03:59 -0800

    begin work on worker and cron

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -25,7 +25,7 @@ exports.initialize = function(pathsObj){
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
-exports.readListOfUrls = function(callback, file) {
+exports.readListOfUrls = function(callback) {
   fs.readFile(exports.paths.list, function(err, data) {
     if(err) {
       throw err;
